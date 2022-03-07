23f0226bffcecd1fdb6d98006a116269dfc00ed7
Charles-Nexient <ckyewalabye@nexient.com>
2022-03-06 19:35:33 -0600

    Demo of implicit and explicit intents

---
+++ app/src/main/java/com/example/charles/walmartapp/RegisterActivity.kt
@@ -0,0 +1,32 @@
+package com.example.charles.walmartapp
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import com.example.charles.walmartapp.databinding.ActivityRegisterBinding
+
+class RegisterActivity : Activity(){
+
+    private lateinit var regBinder: ActivityRegisterBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        regBinder = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(regBinder.root)
+
+        regBinder.signUp.setOnClickListener {
+            val intent = Intent()
+
+            intent.putExtra(
+                "user", User(
+                regBinder.firstname.text.toString(),
+                regBinder.lastname.text.toString(),
+                regBinder.emailAddress.text.toString(),
+                regBinder.pass.text.toString()
+                )
+            )
+            setResult(RESULT_OK, intent)
+            finish()
+        }
+    }
+}
---
+++ app/src/main/java/com/example/charles/walmartapp/ShoppingActivity.kt
@@ -0,0 +1,39 @@
+package com.example.charles.walmartapp
+
+import android.app.Activity
+import android.os.Bundle
+import android.widget.Toast
+import com.example.charles.walmartapp.databinding.ActivityShoppingBinding
+
+class ShoppingActivity : Activity(){
+    private lateinit var shopBinder: ActivityShoppingBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        shopBinder = ActivityShoppingBinding.inflate(layoutInflater)
+        setContentView(shopBinder.root)
+
+        shopBinder.userName.text = "Welcome " + intent.extras?.getString("username")
+
+        shopBinder.clothing.setOnClickListener{
+            toast(ActivityShoppingBinding::clothing.name)
+        }
+
+        shopBinder.electronics.setOnClickListener{
+            toast(ActivityShoppingBinding::electronics.name)
+        }
+
+        shopBinder.beauty.setOnClickListener{
+            toast(ActivityShoppingBinding::beauty.name)
+        }
+
+        shopBinder.food.setOnClickListener {
+            toast(ActivityShoppingBinding::food.name)
+        }
+
+    }
+
+    private fun toast(name: String) {
+        Toast.makeText(this,"Hi there!, your category is $name", Toast.LENGTH_SHORT).show()
+    }
+}
